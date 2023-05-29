b0ae5cf49d0d46e3043ce5e3ebd598d0e9995d72
RakaPanduAulia <rakapandu12@gmail.com>
2023-05-29 12:14:08 +0700

    Test1
    Test

---
+++ services/conf.php
@@ -0,0 +1,20 @@
+<?php
+
+class CONF {
+
+    /* Flag for demo version */
+    public $DEMO_VERSION = false;
+
+    /* Data configuration for database */
+    public $DB_SERVER   = "localhost";
+    public $DB_USER     = "appsians_amin";
+    public $DB_PASSWORD = "appsians_amin";
+    public $DB_NAME     = "appsians_testapi";
+
+    /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
+    /* This string must be same with security code at Android, if its different android unable to submit order */
+    public $SECURITY_CODE = "secure_code";
+
+}
+
+?>
